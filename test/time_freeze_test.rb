f1360d908b57f2c3d4b42bf71bbe2d0668a88d69
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2010-12-02 14:15:48 +0800

    cleaner testing

--- test/time_freeze_test.rb
+++ test/time_freeze_test.rb
@@ -4,6 +4,10 @@ require 'date'
 
 class TimeFreezeTest < Test::Unit::TestCase
   
+  def setup
+    @time_now = Time.now
+  end
+  
   def test_normal_function
      time_before = Time.now
      datetime_before = DateTime.now
@@ -31,13 +35,14 @@ class TimeFreezeTest < Test::Unit::TestCase
   class ExampleRaisedException < Exception; end
   
   def test_freeze_unfreezes_after_exception
-    time_now  = Time.now
-    freeze_to = Time.mktime(2001,12,30,13,45,59,999999)
+   time1 = Time.mktime(2001,12,30,13,45,59,999999)
+    
+    assert_unfrozen_time
+    
     begin
-      TimeFreeze.freeze!(freeze_to) do
-        assert_equal freeze_to, Time.now
-        assert_equal freeze_to.to_datetime, DateTime.now
-        assert_equal freeze_to.to_date, Date.today
+      TimeFreeze.freeze!(time1) do
+        
+        assert_frozen_time(time1)
 
         raise ExampleRaisedException
 
@@ -46,35 +51,47 @@ class TimeFreezeTest < Test::Unit::TestCase
 
     rescue ExampleRaisedException
 
-      assert_not_equal freeze_to, Time.now
-      assert_not_equal freeze_to.to_datetime, DateTime.now
-      assert_not_equal freeze_to.to_date, Date.today
+      assert_unfrozen_time
+      
     end
 
-    assert_equal time_now.to_date, Date.today
+    assert_unfrozen_time
   end
   
   def test_nested_freeze
-    time_now  = Time.now
-    freeze_to1 = Time.mktime(2001,12,30,13,45,59,999999)
-    freeze_to2 = Time.mktime(2015,03,16,23,22,13,764282)
+    time1 = Time.mktime(2001,12,30,13,45,59,999999)
+    time2 = Time.mktime(2015,03,16,23,22,13,764282)
     
-    TimeFreeze.freeze!(freeze_to1) do
-      assert_equal freeze_to1, Time.now
-      assert_equal freeze_to1.to_datetime, DateTime.now
-      assert_equal freeze_to1.to_date, Date.today
+    assert_unfrozen_time
+    
+    TimeFreeze.freeze!(time1) do
+      assert_frozen_time(time1)
       
-      TimeFreeze.freeze!(freeze_to2) do
-        assert_equal freeze_to2, Time.now
-        assert_equal freeze_to2.to_datetime, DateTime.now
-        assert_equal freeze_to2.to_date, Date.today
+      TimeFreeze.freeze!(time2) do
+        assert_frozen_time(time2)
       end
       
-      assert_equal freeze_to1, Time.now
-      assert_equal freeze_to1.to_datetime, DateTime.now
-      assert_equal freeze_to1.to_date, Date.today
+      assert_frozen_time(time1)
     end
-  end    
+    
+    assert_unfrozen_time
+  end
+  
+  def assert_frozen_time(expected_time)
+    assert_equal TimeFreeze.frozen_time,    expected_time
+    assert_equal expected_time,             Time.now
+    assert_equal expected_time.to_datetime, DateTime.now
+    assert_equal expected_time.to_date,     Date.today
+  end
+  
+  def assert_unfrozen_time
+    assert_in_delta 0, Time.now - @time_now, 0.5
+    
+    second_of_day = 1.0 / 24 / 60 / 60 
+    assert_in_delta 0, DateTime.now - @time_now.to_datetime, 0.5*second_of_day
+    
+    assert_equal @time_now.to_date, Date.today
+  end
   
 end
       
